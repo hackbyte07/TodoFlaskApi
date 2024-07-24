fc0e99956109bafe4ae43fe6eac1d74358f3696e
aayush rana <aayushranahardi@gmail.com>
2024-07-24 14:05:13 +0530

    working;

--- src/app.py
+++ src/app.py
@@ -1,15 +1,47 @@
-from pydoc import render_doc
-from flask import Flask, render_template, render_template_string
+from flask import Flask, jsonify, render_template,  render_template_string, request
+
 
 
 app = Flask(__name__)
+
+@app.get('/add')
+def add():
+    return {'name': 'aayush', 'data': {'data': [1, 2, 3]}}
 
 @app.get('/')
 def index():
     return  render_template_string('<h1>welcome to flask 🌶️</h1>')
 
+@app.post('/name/<int:name>')
+def postThis():
+    return 'This is a post request'
 
+@app.patch('/patch')
+def patchThis():
+    return 'This is a patch request'
+
+@app.delete('/delete')
+def deleteThis():
+    return 'The post is deleted.'
+
+@app.get('/template')
+def show_template():
+    return render_template('index.html', listValue = [0, 2, 4, 7, 8])
+
+
+@app.route('/home') 
+def home():       
+        data = "hello world"
+        return jsonify({'data': data}) 
+  
+
+@app.route('/home/<int:num>', methods = ['GET']) 
+def disp(num): 
+  
+    return jsonify({'data': num**2}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    app.logger.debug('logging the application')
+
 
